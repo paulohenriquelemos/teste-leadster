54ecd5832b2f14c0c3a8184a7cf008b6ff9d2c5a
Paulo Henrique de Souza Lemos <ph-lemos@hotmail.com>
2023-06-28 16:20:22 -0300

    add section 01

---
+++ src/components/Section-01.tsx
@@ -0,0 +1,25 @@
+import Image from 'next/image'
+
+import teste from '../assets/asset-header.png'
+
+export function Section01() {
+  return (
+    <div className="grid w-full place-items-center bg-bg-light py-36">
+      <a
+        href="#"
+        className="rounded-full rounded-bl-none border-2 border-primary px-3 py-1 text-xs font-bold uppercase text-primary transition hover:bg-primary hover:text-white"
+      >
+        webinars exclusivos
+      </a>
+      <p className="mt-3 text-4xl text-text-dark">Menos Conversinha,</p>
+      <p className="relative mt-2 border-b px-6 pb-4 text-7xl font-semibold text-primary">
+        Mais Conversão
+        <Image src={teste} alt="" className="absolute -top-1 right-0" />
+      </p>
+      <p className="mt-3 text-text-dark">
+        Conheça as estratégias que <strong>mudaram o jogo</strong> e como
+        aplicálas no seu negócio
+      </p>
+    </div>
+  )
+}
