80749be36f1188630efb8f6581fbae9bf078f08e
Paulo Henrique de Souza Lemos <ph-lemos@hotmail.com>
2023-06-28 15:26:41 -0300

    add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,12 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: '#0084ff',
+        'bg-light': '#f0f8ff',
+        'bg-gray': '#f7f8fb',
+        'text-dark': '#1c3c50',
+        'text-light': '#647598',
       },
     },
   },
