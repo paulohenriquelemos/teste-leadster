ad22ffd9ecc377ee59208b364b338672c8c36c75
Paulo Henrique de Souza Lemos <ph-lemos@hotmail.com>
2023-06-29 13:36:13 -0300

    add responsive section 01

--- src/components/Section-01.tsx
+++ src/components/Section-01.tsx
@@ -5,22 +5,28 @@ import teste from '../assets/asset-header.png'
 
 export function Section01() {
   return (
-    <div className="grid w-full place-items-center bg-bg-light py-36">
+    <section className="grid w-full place-items-center bg-bg-light px-8 py-36">
       <Link
         href="#"
         className="rounded-full rounded-bl-none border-2 border-primary px-3 py-1 text-xs font-bold uppercase text-primary transition hover:bg-primary hover:text-white"
       >
         webinars exclusivos
       </Link>
-      <p className="mt-3 text-4xl text-text-dark">Menos Conversinha,</p>
-      <p className="relative mt-2 border-b px-6 pb-4 text-7xl font-semibold text-primary">
-        Mais Conversão
-        <Image src={teste} alt="" className="absolute -top-1 right-0" />
+      <p className="mt-3 text-2xl text-text-dark md:text-4xl">
+        Menos Conversinha,
       </p>
-      <p className="mt-3 text-text-dark">
+      <p className="relative mt-2 border-b px-6 pb-4 text-4xl font-semibold text-primary md:text-7xl">
+        Mais Conversão
+        <Image
+          src={teste}
+          alt=""
+          className="absolute -top-1 right-0 w-9 md:w-12"
+        />
+      </p>
+      <p className="mt-3 text-center text-text-dark">
         Conheça as estratégias que <strong>mudaram o jogo</strong> e como
         aplicálas no seu negócio
       </p>
-    </div>
+    </section>
   )
 }
