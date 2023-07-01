3be7977672a873eb35436e22852b54c2c8730f43
Paulo Henrique de Souza Lemos <ph-lemos@hotmail.com>
2023-06-30 22:54:59 -0300

    change container

--- src/components/Section-02.tsx
+++ src/components/Section-02.tsx
@@ -63,73 +63,75 @@ export function Section02() {
 
   return (
     <section className="p-8">
-      <div className="flex justify-between gap-3 border-b pb-3">
-        <div className="flex flex-wrap items-center gap-3">
-          <ButtonBranch
-            onPress={() => handleChangeBranch('agencias')}
-            title="Agências"
-            active={branch === 'agencias'}
-          />
-          <ButtonBranch
-            onPress={() => handleChangeBranch('chatbot')}
-            title="Chatbot"
-            active={branch === 'chatbot'}
-          />
-          <ButtonBranch
-            onPress={() => handleChangeBranch('marketing_digital')}
-            title="Marketin Digital"
-            active={branch === 'marketing_digital'}
-          />
-          <ButtonBranch
-            onPress={() => handleChangeBranch('geracao_leads')}
-            title="Geração de Leads"
-            active={branch === 'geracao_leads'}
-          />
-          <ButtonBranch
-            onPress={() => handleChangeBranch('midia_paga')}
-            title="Mídiga Paga"
-            active={branch === 'midia_paga'}
-          />
-        </div>
-        <div className="space-x-2 whitespace-nowrap">
-          <span className="mb-2 block text-end text-sm font-bold text-text-dark sm:mb-0 sm:inline">
-            Ordenar por
-          </span>
-          <select
-            name=""
-            id=""
-            onChange={handleChangeOrder}
-            value={selected}
-            className="rounded-lg border border-text-dark p-1 text-sm"
-          >
-            <option value=""></option>
-            <option value="createdAt">Data de publicação</option>
-            <option value="outro" disabled>
-              Outro método
-            </option>
-          </select>
-        </div>
-      </div>
-      <Swiper
-        pagination={pagination}
-        modules={[Pagination]}
-        className="mySwiper"
-      >
-        {data.slice(0, paginationNumber).map((page, index) => {
-          return (
-            <SwiperSlide
-              key={page.id}
-              className="!grid gap-5 sm:grid-cols-2 lg:grid-cols-3"
+      <div className="container mx-auto">
+        <div className="flex justify-between gap-3 border-b pb-3">
+          <div className="flex flex-wrap items-center gap-3">
+            <ButtonBranch
+              onPress={() => handleChangeBranch('agencias')}
+              title="Agências"
+              active={branch === 'agencias'}
+            />
+            <ButtonBranch
+              onPress={() => handleChangeBranch('chatbot')}
+              title="Chatbot"
+              active={branch === 'chatbot'}
+            />
+            <ButtonBranch
+              onPress={() => handleChangeBranch('marketing_digital')}
+              title="Marketin Digital"
+              active={branch === 'marketing_digital'}
+            />
+            <ButtonBranch
+              onPress={() => handleChangeBranch('geracao_leads')}
+              title="Geração de Leads"
+              active={branch === 'geracao_leads'}
+            />
+            <ButtonBranch
+              onPress={() => handleChangeBranch('midia_paga')}
+              title="Mídiga Paga"
+              active={branch === 'midia_paga'}
+            />
+          </div>
+          <div className="space-x-2 whitespace-nowrap">
+            <span className="mb-2 block text-end text-sm font-bold text-text-dark sm:mb-0 sm:inline">
+              Ordenar por
+            </span>
+            <select
+              name=""
+              id=""
+              onChange={handleChangeOrder}
+              value={selected}
+              className="rounded-lg border border-text-dark p-1 text-sm"
             >
-              {data
-                .slice(cardPerPage * index, cardPerPage * index + cardPerPage)
-                .map((card: CardProps) => {
-                  return <Card key={card.id} card={card} />
-                })}
-            </SwiperSlide>
-          )
-        })}
-      </Swiper>
+              <option value=""></option>
+              <option value="createdAt">Data de publicação</option>
+              <option value="outro" disabled>
+                Outro método
+              </option>
+            </select>
+          </div>
+        </div>
+        <Swiper
+          pagination={pagination}
+          modules={[Pagination]}
+          className="mySwiper"
+        >
+          {data.slice(0, paginationNumber).map((page, index) => {
+            return (
+              <SwiperSlide
+                key={page.id}
+                className="!grid gap-5 sm:grid-cols-2 lg:grid-cols-3"
+              >
+                {data
+                  .slice(cardPerPage * index, cardPerPage * index + cardPerPage)
+                  .map((card: CardProps) => {
+                    return <Card key={card.id} card={card} />
+                  })}
+              </SwiperSlide>
+            )
+          })}
+        </Swiper>
+      </div>
     </section>
   )
 }
